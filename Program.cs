8db9f568c705b1b3612d851a7b904cbf180d42b6
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-12 17:14:57 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             //Console.WriteLine("1 For TATA Company Employee Wage");
@@ -20,18 +20,15 @@
             //        EmployeeWageComputation wagecomputation = new EmployeeWageComputation();
             //        wagecomputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
             //        wagecomputation.CalculateEmpWage("MAHINDRA");
-
             //        break;
+            //}
+            EmployeeWageComputation employeeComputation = new EmployeeWageComputation(2);
+            employeeComputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+            employeeComputation.CalculateEmpWage("TATA");
+            employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+            employeeComputation.CalculateEmpWage("MAHINDRA");
 
-                    EmployeeWageComputation employeeComputation = new EmployeeWageComputation(2);
-                    employeeComputation.AddCompany("TATA", 20, 8, 4, 100, 20);
-                    employeeComputation.CalculateEmpWage("TATA");
-                    employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
-                    employeeComputation.CalculateEmpWage("MAHINDRA");
-
-                    employeeComputation.displayArray();
-
-            }
+            employeeComputation.displayArray();
         }
     }
-
+}
